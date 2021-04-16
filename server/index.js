e42d2f8e18911a4c00af45161a9e865fb42bd424
NielsPeeters96 <78353674+NielsPeeters96@users.noreply.github.com>
2021-04-16 10:15:35 +0200

    Update

---
+++ server/index.js
@@ -0,0 +1,78 @@
+const needle = require('needle')
+const config = require('dotenv').config()
+const TOKEN = process.env.TWITTER_BEARER_TOKEN
+
+const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules'
+const streamURL = 'https://api.twitter.com/2/tweets/search/stream?tweet.field=public_metrics$expansions=author_id'
+
+const rules = [{value: 'giveaway'}, {value: 'xbox'}]
+
+// Get Rules
+async function getRules() {
+    const response = await needle('get', rulesURL, {
+        headers: {
+            Authorization: `Bearer ${TOKEN}`,
+        },
+    })
+
+    console.log(response.body);
+    return response.body
+};
+
+// Set Rules
+async function setRules() {
+    const data = {
+        add: rules
+    }
+
+    const response = await needle('post', rulesURL, data, {
+        headers: {
+            'content-type': 'application/json',
+            Authorization: `Bearer ${TOKEN}`,
+        },
+    })
+
+    return response.body
+};
+
+// Delete Rules
+async function deleteRules(rules) {
+    if(!Array.isArray(rules.data)) {
+        return null
+    }
+
+    const ids = rules.data.map((rule) => rule.id)
+
+    const data = {
+        delete: {
+            ids: ids
+        }
+    }
+
+    const response = await needle('post', rulesURL, data, {
+        headers: {
+            'content-type': 'application/json',
+            Authorization: `Bearer ${TOKEN}`,
+        },
+    })
+
+    return response.body
+};
+
+(async() => {
+    let currentRules
+    
+    try {
+        // Get all rules
+        currentRules = await getRules()
+
+        // Clean all rules
+        await deleteRules(currentRules)
+
+        // Set rules
+        await setRules()
+    } catch (error) {
+        console.error(error)
+        process.exit(1)
+    }
+})()
